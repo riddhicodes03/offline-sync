02a242ebb5dd1c9a11b51d4691cb720ea85172f5
Riddhi Sundar Sahu <riddhisundarsahu0498@gmail.com>
2026-08-20 00:24:51 +0530

    mysql syntax error fixed

--- server/src/model/user.model.js
+++ server/src/model/user.model.js
@@ -1,10 +1,10 @@
 const {getPool}=require('../config/db');
 
 exports.findByEmail = async(email)=>{
-    const [rows] = await getPool().query(`SELECT * from USER WHERE email =?`,[email]);
+    const [rows] = await getPool().query(`SELECT * from users WHERE email =?`,[email]);
     return rows[0];
 }
 exports.create = async({email,password,name})=>{
-    const [result]= await getPool().query('INSERT INTO user(name,email,password) VALUES(?,?,?)',[name,email,password]);
+    const [result]= await getPool().query('INSERT INTO users(name,email,password) VALUES(?,?,?)',[name,email,password]);
     return result.insertId;
 }
--- server/src/schema/userSchema.js
+++ server/src/schema/userSchema.js
@@ -1,12 +1,12 @@
 const {getPool}=require('../config/db');
 async function createUsersTable(){
 await getPool().query(  `
-    CREATE TABLE IF NOT EXISTS user(
-        id INT AUTO INCREMENT PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(100) NOT NULL,
-        password VARCHAR(255)NOT NULL,
-    )
+    CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
+)
   `);
 }
 
