9114f8f42b8c9ba0a386f1bd3973439af56ca8db
Riddhi Sundar Sahu <riddhisundarsahu0498@gmail.com>
2026-08-20 00:30:23 +0530

    interpolation fix

--- server/server.js
+++ server/server.js
@@ -15,5 +15,5 @@ app.get('/',(req,res)=>{
 })
 const PORT = process.env.PORT || 3000;
 initDB().then(()=>createAllTables()).then(()=>{
-    app.listen( PORT,() =>console.log("Server running on port ${PORT}"));
+    app.listen( PORT,() =>console.log(`Server running on port ${PORT}`));
 });
